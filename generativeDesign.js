b6358d131ca7d94241fa2fd946093b3834c19a1a
Bichiran Gondal <bichiran.gondal@student.ehb.be>
2024-11-14 04:53:27 +0100

    milestone2 gemaakt

--- generativeDesign.js
+++ generativeDesign.js
@@ -1,24 +1,32 @@
 "use strict";
-import context from "../dev1-courseproject-BichiranGondal/scripts/context.js";
-import * as Utils from "../dev1-courseproject-BichiranGondal/scripts/utils.js";
-import * as Noise from "../dev1-courseproject-BichiranGondal/scripts/noise.js";
+import context from "./scripts/context.js";
+import * as Utils from "./scripts/utils.js";
 
-draw();
+let width = context.canvas.width;
+let height = context.canvas.height;
 
-function draw() {
-    context.lineWidth = 2;
-    drawGradient();
-    context.strokeStyle = "black";
-    context.strokeRect(50, 50, 300, 300);
+let gradient = context.createLinearGradient(0, 0, width, height);
+gradient.addColorStop(0, Utils.rgb(Utils.randomNumber(1, 255), Utils.randomNumber(1, 255), Utils.randomNumber(1, 255)));
+gradient.addColorStop(0.5, Utils.rgb(Utils.randomNumber(1, 255), Utils.randomNumber(1, 255), Utils.randomNumber(1, 255)));
+gradient.addColorStop(1, Utils.rgb(Utils.randomNumber(1, 255), Utils.randomNumber(1, 255), Utils.randomNumber(1, 255)));
 
+backgroundColor();
+
+function backgroundColor() {
+   context.fillStyle = gradient;
+   context.fillRect(0, 0, width, height);
+   drawCircle();
 }
 
-function drawGradient() {
-    let i = 0;
-    while (i <= 300) {
-        let gray = i / 300 * 255;
-        context.strokeStyle = Utils.rgb(gray, gray, gray);
-        Utils.drawLine(50 + i, 50, 50 + i, 350);
-        i++;
-    }
+function drawCircle() {
+   for (let i = 0; i <= 20; i++){
+      for (let j = 0; j <= 20; j++){
+         context.fillStyle = Utils.hsla(0, 0, 100, 50);
+         Utils.fillCircle(i * 100, j * 100, 50);
+         Utils.fillCircle(i * 100, j * 100, 40);
+         Utils.fillCircle(i * 100, j * 100, 30);
+         Utils.fillCircle(i * 100, j * 100, 20);
+         Utils.fillCircle(i * 100, j * 100, 10);
+      }
+   }
 }
