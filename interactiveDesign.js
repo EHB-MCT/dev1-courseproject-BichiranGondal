5a80e494557020bc3b44ec4a709402e1fe2c2606
Bichiran Gondal <bichiran.gondal@student.ehb.be>
2024-12-19 00:46:05 +0100

    background

--- interactiveDesign.js
+++ interactiveDesign.js
@@ -5,32 +5,23 @@ import * as Utils from "./scripts/utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-let gradient = context.createLinearGradient(0, 0, width, height);
-gradient.addColorStop(0, Utils.rgb(Utils.randomNumber(1, 255), Utils.randomNumber(1, 255), Utils.randomNumber(1, 255)));
-gradient.addColorStop(0.5, Utils.rgb(Utils.randomNumber(1, 255), Utils.randomNumber(1, 255), Utils.randomNumber(1, 255)));
-gradient.addColorStop(1, Utils.rgb(Utils.randomNumber(1, 255), Utils.randomNumber(1, 255), Utils.randomNumber(1, 255)));
+let gradient = context.createLinearGradient(0, 0, 0, height);
+gradient.addColorStop(0, "#010131");
+gradient.addColorStop(1, "#450085");
 
-backgroundColor();
+background();
+drawStars();
 
-function backgroundColor() {
+
+function background() {
    context.fillStyle = gradient;
    context.fillRect(0, 0, width, height);
-   drawCircle();
 }
 
-function drawCircle() {
-   for (let i = 0; i <= 20; i++){
-      for (let j = 0; j <= 20; j++){
-         let radius = 50;
-         context.fillStyle = Utils.hsla(0, 0, 100, 40);
-         Utils.fillCircle(i * 100, j * 100, 50);
-         Utils.fillCircle(i * 100, j * 100, 45);
-         Utils.fillCircle(i * 100, j * 100, 40);
-         Utils.fillCircle(i * 100, j * 100, 35);
-         Utils.fillCircle(i * 100, j * 100, 30);
-         Utils.fillCircle(i * 100, j * 100, 25);
-         Utils.fillCircle(i * 100, j * 100, 20);
-         Utils.fillCircle(i * 100, j * 100, 10);
-      }
+
+function drawStars() {
+   for (let i = 1; i < 150; i++) {
+      context.fillStyle = "#FFD354";
+      Utils.fillCircle(Utils.randomNumber(0, width), Utils.randomNumber(0, height), Utils.randomNumber(2, 5));
    }
 }
