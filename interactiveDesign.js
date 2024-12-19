d69c267f0d9fad34dd747d002297e65659e5613d
Bichiran Gondal <bichiran.gondal@student.ehb.be>
2024-12-19 01:26:40 +0100

    array, object toegevoegd voor sterren

--- interactiveDesign.js
+++ interactiveDesign.js
@@ -9,19 +9,57 @@ let gradient = context.createLinearGradient(0, 0, 0, height);
 gradient.addColorStop(0, "#010131");
 gradient.addColorStop(1, "#450085");
 
-background();
-drawStars();
+let stars = [];
 
+background();
+fillSky();
+animate();
 
 function background() {
    context.fillStyle = gradient;
    context.fillRect(0, 0, width, height);
 }
 
+function fillSky() {
+   for (let i = 0; i < 150; i++) {
+      let star = {
+         x: Utils.randomNumber(0, width),
+         y: Utils.randomNumber(0, height),
+         r: Utils.randomNumber(2, 5),
+         speed: Utils.randomNumber(0.01, 0.05)
+      };
+      stars.push(star);
+   }
+}
 
 function drawStars() {
-   for (let i = 1; i < 150; i++) {
-      context.fillStyle = "#F1F781";
-      Utils.fillCircle(Utils.randomNumber(0, width), Utils.randomNumber(0, height), Utils.randomNumber(2, 5));
+   context.fillStyle = "#F1F781";
+   for (let i = 0; i < stars.length; i++) {
+      let star2 = stars[i];
+      Utils.fillCircle(star2.x, star2.y, star2.r);
    }
+}
+
+function updateStars(mouseX, mouseY) {
+   for (let i = 0; i < stars.length; i++) {
+      let star2 = stars[i];
+      star2.x += (mouseX - star2.x) * star2.speed;
+      star2.y += (mouseY - star2.y) * star2.speed;
+   }
+}
+
+function animate() {
+   context.clearRect(0, 0, width, height); // de canvas leegmaken
+   background(); // de background opnieuw tekenen
+   drawStars(); // de sterren tekenen
+   requestAnimationFrame(animate); //animation herhalen
+}
+
+/**
+ * 
+ * @param {MouseEvent} eventData 
+ */
+function moveStars(eventData) {
+   let mouseX = eventData.pageX;
+   let mouseY = eventData.pageY;
 }
